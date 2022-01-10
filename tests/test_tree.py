9caa8b9a854c1a9189ea661050feea53f2a39440
Ruslan Kuprieiev <kupruser@gmail.com>
2022-01-10 18:18:27 +0200

    dvc: separate data and objects
    Initial simple separation to get the process of extracting dvc-objects
    and dvc-data packages out of dvc.
    
    dvc-objects is for:
     * defining base Object (aka HashFile) and ObjectDB
     * providing tools to work with those objects (check/transfer/etc)
     * etc
    
    dvc-data is for:
     * defining higher level objects (e.g. Tree)
     * bulding objects from the workspace (aka staging)
     * projecting objects back into the workspace (aka checkout)
     * comparing Tree objects (aka diff)
     * defining misc object databases (e.g LocalObjectDB)
     * higher-level transfer (e.g. with Tree indexes)
     * etc
    
    For example, the objects we use in run-cache are based on dvc-objects
    and not on dvc-data, as they have absolutely nothing to do with data
    management.
    
    There are still some mixups in the code that will be addressed in the
    followups.

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,9 +2,9 @@ from operator import itemgetter
 
 import pytest
 
+from dvc.data.meta import Meta
+from dvc.data.tree import Tree, _merge
 from dvc.hash_info import HashInfo
-from dvc_objects.meta import Meta
-from dvc_objects.tree import Tree, _merge
 
 
 @pytest.mark.parametrize(
