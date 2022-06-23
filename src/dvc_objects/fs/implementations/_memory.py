a805843f90944392d7bdf6e886a567a74f181164
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-06-23 12:41:51 +0545

    memfs2: make it non-cacheable, add tests, minor refactoring

--- src/dvc_objects/fs/implementations/_memory.py
+++ src/dvc_objects/fs/implementations/_memory.py
@@ -1,4 +1,5 @@
 import errno
+import os
 
 from fsspec import AbstractFileSystem
 from fsspec.implementations.memory import MemoryFile
@@ -6,8 +7,9 @@ from fsspec.implementations.memory import MemoryFileSystem as MemFS
 
 
 class MemFS2(AbstractFileSystem):  # pylint: disable=abstract-method
-    """In-Memory Object Storage FileSystem."""
+    """In-Memory Object Storage FileSystem based on Trie data-structure."""
 
+    cachable = False
     protocol = "memory"
     root_marker = "/"
     _strip_protocol = MemFS._strip_protocol  # pylint: disable=protected-access
@@ -20,19 +22,14 @@ class MemFS2(AbstractFileSystem):  # pylint: disable=abstract-method
 
     def ls(self, path, detail=False, **kwargs):
         path = self._strip_protocol(path)
-        if path not in ("", "/"):
-            info = self.info(path)
-            if info["type"] != "directory":
-                return [info] if detail else [path]
+        out = []
 
-        ret = []
-
-        def node_factory(path_conv, paths, children, *args):
-            node_path = path_conv(paths)
-            if path == node_path:
-                list(filter(None, children))
+        def node_factory(path_conv, parts, children, _filelike=None):
+            node_path = path_conv(parts)
+            if path == node_path and children:
+                list(children)
             else:
-                ret.append(node_path)
+                out.append(node_path)
 
         try:
             self.trie.traverse(node_factory, prefix=path)
@@ -44,45 +41,8 @@ class MemFS2(AbstractFileSystem):  # pylint: disable=abstract-method
             ) from exc
 
         if not detail:
-            return ret
-
-        return [self.info(_path) for _path in ret]
-
-    def _rm(self, path):
-        path = self._strip_protocol(path)
-        if self.isdir(path):
-            raise IsADirectoryError(errno.EISDIR, "Is a directory", path)
-
-        try:
-            del self.trie[path]
-        except KeyError as e:
-            raise FileNotFoundError(errno.ENOENT, "No such file", path) from e
-
-    def _open(  # pylint: disable=arguments-differ
-        self, path, mode="rb", **kwargs
-    ):
-        path = self._strip_protocol(path)
-        if self.isdir(path):
-            raise IsADirectoryError(errno.EISDIR, "Is a directory", path)
-        short = self.trie.shortest_prefix(path)
-        if short and short.key != path:
-            raise NotADirectoryError(errno.ENOTDIR, "Not a directory", path)
-
-        if mode in ["rb", "ab", "rb+"]:
-            if filelike := self.trie.get(path):
-                if mode == "ab":
-                    # position at the end of file
-                    filelike.seek(0, 2)
-                else:
-                    # position at the beginning of file
-                    filelike.seek(0)
-                return filelike
-            raise FileNotFoundError(errno.ENOENT, "No such file", path)
-        if mode == "wb":
-            filelike = MemoryFile(self, path)
-            if not self._intrans:
-                filelike.commit()
-            return filelike
+            return out
+        return [self.info(p) for p in out]
 
     def info(self, path, **kwargs):
         path = self._strip_protocol(path)
@@ -104,13 +64,53 @@ class MemFS2(AbstractFileSystem):  # pylint: disable=abstract-method
             }
         raise FileNotFoundError(errno.ENOENT, "No such file", path)
 
+    def _rm(self, path):
+        path = self._strip_protocol(path)
+        if self.isdir(path):
+            raise IsADirectoryError(errno.EISDIR, "Is a directory", path)
+
+        try:
+            del self.trie[path]
+        except KeyError as e:
+            raise FileNotFoundError(errno.ENOENT, "No such file", path) from e
+
+    def rm(self, path, recursive=False, maxdepth=None):
+        paths = self.expand_path(path, recursive=recursive, maxdepth=maxdepth)
+        for p in paths:
+            self.store.pop(p, None)
+
+    def _open(  # pylint: disable=arguments-differ
+        self, path, mode="rb", **kwargs
+    ):
+        path = self._strip_protocol(path)
+        if self.isdir(path):
+            raise IsADirectoryError(errno.EISDIR, "Is a directory", path)
+        short = self.trie.shortest_prefix(path)
+        if short and short.key != path:
+            raise NotADirectoryError(errno.ENOTDIR, "Not a directory", path)
+
+        if mode in ["rb", "ab", "rb+"]:
+            if filelike := self.trie.get(path):
+                filelike.seek(0, os.SEEK_END if mode == "ab" else os.SEEK_SET)
+                return filelike
+            raise FileNotFoundError(errno.ENOENT, "No such file", path)
+
+        assert mode == "wb"
+        filelike = MemoryFile(self, path)
+        if not self._intrans:
+            filelike.commit()
+        return filelike
+
     def cp_file(self, path1, path2, **kwargs):
         path1 = self._strip_protocol(path1)
         path2 = self._strip_protocol(path2)
-        if self.isdir(path1):
+
+        try:
+            src = self.open(path1, "rb")
+        except IsADirectoryError:
             return
 
-        with self.open(path1, "rb") as src, self.open(path2, "wb") as dst:
+        with src, self.open(path2, "wb") as dst:
             dst.write(src.getbuffer())
 
     def created(self, path):
