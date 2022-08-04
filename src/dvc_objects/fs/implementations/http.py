805a716d84c26b8a775c4e8852f02124da12e2ed
Sigurd Spieckermann <2206639+sisp@users.noreply.github.com>
2022-08-04 10:55:39 +0200

    fs: http: fix `isfile` method (#116)

--- src/dvc_objects/fs/implementations/http.py
+++ src/dvc_objects/fs/implementations/http.py
@@ -154,9 +154,6 @@ class HTTPFileSystem(FileSystem):
     def isdir(self, *args, **kwargs):
         return False
 
-    def isfile(self, *args, **kwargs):
-        return True
-
     def ls(self, *args, **kwargs):
         raise NotImplementedError
 
