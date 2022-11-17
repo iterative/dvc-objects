6bcf24e9147a1c813334fa972383249a2e268613
Peter Rowlands <peter@pmrowla.com>
2022-11-17 16:22:25 +0900

    mypy: add type hints for fs.path

--- src/dvc_objects/fs/path.py
+++ src/dvc_objects/fs/path.py
@@ -1,39 +1,46 @@
 import ntpath
 import posixpath
+from types import ModuleType
+from typing import Callable, Iterable, Iterator, Optional, Sequence, Tuple
 from urllib.parse import urlsplit, urlunsplit
 
 
 class Path:
-    def __init__(self, sep, getcwd=None, realpath=None):
-        def _getcwd():
+    def __init__(
+        self,
+        sep: str,
+        getcwd: Optional[Callable[[], str]] = None,
+        realpath: Optional[Callable[[str], str]] = None,
+    ):
+        def _getcwd() -> str:
             return ""
 
-        self.getcwd = getcwd or _getcwd
+        self.getcwd: Callable[[], str] = getcwd or _getcwd
         self.realpath = realpath or self.abspath
 
         if sep == posixpath.sep:
-            self.flavour = posixpath
+            self.flavour: ModuleType = posixpath
         elif sep == ntpath.sep:
             self.flavour = ntpath
         else:
             raise ValueError(f"unsupported separator '{sep}'")
 
-    def chdir(self, path):
-        def _getcwd():
+    def chdir(self, path: str):
+        def _getcwd() -> str:
             return path
 
         self.getcwd = _getcwd
 
-    def join(self, *parts):
+    def join(self, *parts: str) -> str:
         return self.flavour.join(*parts)
 
-    def split(self, path):
+    def split(self, path: str) -> Tuple[str, str]:
         return self.flavour.split(path)
 
-    def splitext(self, path):
+    def splitext(self, path: str) -> Tuple[str, str]:
         return self.flavour.splitext(path)
 
-    def normpath(self, path):
+    def normpath(self, path: str) -> str:
         if self.flavour == ntpath:
             return self.flavour.normpath(path)
 
@@ -41,21 +48,21 @@ class Path:
         parts[2] = self.flavour.normpath(parts[2])
         return urlunsplit(parts)
 
-    def isabs(self, path):
+    def isabs(self, path: str) -> bool:
         return self.flavour.isabs(path)
 
-    def abspath(self, path):
+    def abspath(self, path: str) -> str:
         if not self.isabs(path):
             path = self.join(self.getcwd(), path)
         return self.normpath(path)
 
-    def commonprefix(self, path):
-        return self.flavour.commonprefix(path)
+    def commonprefix(self, paths: Sequence[str]) -> str:
+        return self.flavour.commonprefix(paths)
 
-    def commonpath(self, paths):
+    def commonpath(self, paths: Iterable[str]) -> str:
         return self.flavour.commonpath(paths)
 
-    def parts(self, path):
+    def parts(self, path: str) -> Tuple[str, ...]:
         drive, path = self.flavour.splitdrive(path.rstrip(self.flavour.sep))
 
         ret = []
@@ -78,13 +85,13 @@ class Path:
 
         return tuple(ret)
 
-    def parent(self, path):
+    def parent(self, path: str) -> str:
         return self.flavour.dirname(path)
 
-    def dirname(self, path):
+    def dirname(self, path: str) -> str:
         return self.parent(path)
 
-    def parents(self, path):
+    def parents(self, path: str) -> Iterator[str]:
         while True:
             parent = self.flavour.dirname(path)
             if parent == path:
@@ -92,21 +99,21 @@ class Path:
             yield parent
             path = parent
 
-    def name(self, path):
+    def name(self, path: str) -> str:
         return self.flavour.basename(path)
 
-    def suffix(self, path):
+    def suffix(self, path: str) -> str:
         name = self.name(path)
         _, dot, suffix = name.partition(".")
         return dot + suffix
 
-    def with_name(self, path, name):
+    def with_name(self, path: str, name: str) -> str:
         return self.join(self.parent(path), name)
 
-    def with_suffix(self, path, suffix):
+    def with_suffix(self, path: str, suffix: str) -> str:
         return self.splitext(path)[0] + suffix
 
-    def isin(self, left, right):
+    def isin(self, left: str, right: str) -> bool:
         if left == right:
             return False
         try:
@@ -116,22 +123,24 @@ class Path:
             return False
         return common == right
 
-    def isin_or_eq(self, left, right):
+    def isin_or_eq(self, left: str, right: str) -> bool:
         return left == right or self.isin(left, right)
 
-    def overlaps(self, left, right):
+    def overlaps(self, left: str, right: str) -> bool:
         # pylint: disable=arguments-out-of-order
         return self.isin_or_eq(left, right) or self.isin(right, left)
 
-    def relpath(self, path, start=None):
+    def relpath(self, path: str, start: Optional[str] = None) -> str:
         if start is None:
             start = "."
         return self.flavour.relpath(
             self.abspath(path), start=self.abspath(start)
         )
 
-    def relparts(self, path, start=None):
+    def relparts(
+        self, path: str, start: Optional[str] = None
+    ) -> Tuple[str, ...]:
         return self.parts(self.relpath(path, start=start))
 
-    def as_posix(self, path):
+    def as_posix(self, path: str) -> str:
         return path.replace(self.flavour.sep, posixpath.sep)
