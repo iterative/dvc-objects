iterative/dvc-objects

commit a0a173926e90ee52d8a2b5bfca333db9f19e0420

fs: transfer: don't return on first exception
Regression from #180

src/dvc_objects/fs/generic.py
@@ -293,7 +293,7 @@ def transfer(
     # copy, the remaining copy operations will be batched.
     for i, (from_p, to_p) in enumerate(zip(from_path, to_path)):
         if links[0] == "copy":
-            return copy(
+            copy(
                 from_fs,
                 from_path[i:],
                 to_fs,
@@ -302,6 +302,7 @@ def transfer(
                 batch_size=batch_size,
                 on_error=on_error,
             )
+            continue
         try:
             _try_links(
                 links,
@@ -325,7 +326,7 @@ def transfer(
                 and isinstance(exc.__context__, FileExistsError)
             ):
                 logger.debug("'%s' file already exists, skipping", to_path)
-                return None
+                continue
 
             if on_error is not None:
                 on_error(from_p, to_p, exc)

commit c5d4a8524fb0625a170b29aa97df9f3129d83cf1

generic: return when done copying
Regression from https://github.com/iterative/dvc-objects/pull/199 🤦

src/dvc_objects/fs/generic.py
@@ -302,7 +302,7 @@ def transfer(
                 batch_size=batch_size,
                 on_error=on_error,
             )
-            continue
+            return
         try:
             _try_links(
                 links,