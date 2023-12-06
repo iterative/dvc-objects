d7fe0a6b4e21826c2830f42aebd5a475910241b8
skshetry <18718008+skshetry@users.noreply.github.com>
2023-12-06 13:02:06 +0545

    refactor benchmark to not create test files for every link type (#243)
    * refactor benchmark to not create test files for every link type
    
    * provide better skip reason

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,14 +11,24 @@ def memfs():
     return MemoryFileSystem(global_store=False)
 
 
+@pytest.fixture(scope="session")
+def make_tmp_dir_pytest_cache(request):
+    cache = request.config.cache
+    path = cache.mkdir("dvc_objects_tests")
+
+    def inner(name=None):
+        tmp_dir = TemporaryDirectory(prefix=name, dir=path)
+        request.addfinalizer(tmp_dir.cleanup)
+        return Path(tmp_dir.name)
+
+    return inner
+
+
 @pytest.fixture
-def tmp_dir_pytest_cache(request):
+def tmp_dir_pytest_cache(make_tmp_dir_pytest_cache):
     """Create a test directory within cache directory.
 
     The cache directory by default, is in the root of the repo, where reflink
     may be supported.
     """
-    cache = request.config.cache
-    path = cache.mkdir("dvc_objects_tests")
-    with TemporaryDirectory(dir=path) as tmp_dir:
-        yield Path(tmp_dir)
+    return make_tmp_dir_pytest_cache("tmp")
