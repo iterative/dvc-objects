a858f4019ffd5ea1680ea4aec96d138611cc321f
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2024-02-06 14:47:58 +0545

    drop tqdm implementation and tqdm callback

--- src/dvc_objects/fs/callbacks.py
+++ src/dvc_objects/fs/callbacks.py
@@ -1,20 +1,12 @@
 from functools import wraps
-from typing import TYPE_CHECKING, BinaryIO, Optional, Type, cast
-
-import fsspec
-from fsspec.callbacks import DEFAULT_CALLBACK, Callback, NoOpCallback
+from typing import TYPE_CHECKING, BinaryIO, cast
 
 if TYPE_CHECKING:
-    from typing import Union
-
-    from tqdm import tqdm
-
-
-__all__ = ["Callback", "NoOpCallback", "TqdmCallback", "DEFAULT_CALLBACK"]
+    import fsspec
 
 
 class CallbackStream:
-    def __init__(self, stream, callback: Callback):
+    def __init__(self, stream, callback: "fsspec.Callback"):
         self.stream = stream
 
         @wraps(stream.read)
@@ -29,28 +21,5 @@ class CallbackStream:
         return getattr(self.stream, attr)
 
 
-class TqdmCallback(fsspec.callbacks.TqdmCallback):
-    def __init__(
-        self,
-        size: Optional[int] = None,
-        value: int = 0,
-        progress_bar: Optional["tqdm"] = None,
-        tqdm_cls: Optional[Type["tqdm"]] = None,
-        **tqdm_kwargs,
-    ):
-        from dvc_objects._tqdm import Tqdm
-
-        tqdm_kwargs.pop("total", None)
-        super().__init__(
-            tqdm_kwargs=tqdm_kwargs, tqdm_cls=tqdm_cls or Tqdm, size=size, value=value
-        )
-        if progress_bar is not None:
-            self.tqdm = progress_bar
-
-    def branched(self, path_1: "Union[str, BinaryIO]", path_2: str, **kwargs):
-        desc = path_1 if isinstance(path_1, str) else path_2
-        return TqdmCallback(bytes=True, desc=desc)
-
-
-def wrap_file(file, callback: Callback) -> BinaryIO:
+def wrap_file(file, callback: "fsspec.Callback") -> BinaryIO:
     return cast(BinaryIO, CallbackStream(file, callback))
