babf8d72cae0e531b3cbea457a8ba3a4bd85d004
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2024-08-15 11:09:03 +0545

    run ci on py313

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,9 @@ locations = "src", "tests"
 pip_dev_flags = ["--use-pep517"]  # reflink package is still missing wheels
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "pypy3.8", "pypy3.9"])
+@nox.session(
+    python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "pypy3.8", "pypy3.9"]
+)
 def tests(session: nox.Session) -> None:
     session.install(".[tests]", *pip_dev_flags)
     session.run(
