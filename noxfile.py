634377cbc3fd208c28984f308194125280baea2d
skshetry <18718008+skshetry@users.noreply.github.com>
2025-07-15 23:05:35 +0545

    add 3.14 support (#344)

--- noxfile.py
+++ noxfile.py
@@ -11,10 +11,12 @@ locations = "src", "tests"
 
 pip_dev_flags = ["--use-pep517"]  # reflink package is still missing wheels
 
+project = nox.project.load_toml()
+python_versions = nox.project.python_versions(project)
+pypy_versions = ["pypy3.9", "pypy3.10", "pypy3.11"]
 
-@nox.session(
-    python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "pypy3.8", "pypy3.9"]
-)
+
+@nox.session(python=python_versions + pypy_versions)
 def tests(session: nox.Session) -> None:
     session.install(".[tests]", *pip_dev_flags)
     session.run(
